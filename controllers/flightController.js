af8bd90d9ea91d12ca1e2936ccf3df504f63e0f5
Igba Simon <jsimonigba@gmail.com>
2022-11-05 01:58:09 +0100

    new work

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,6 @@
 exports.example = (req, res) => {
     console.log("example")
-    res.send("Flight example")
+    res.send("flight booked")
 }
 
 
--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const { json } = require("express");
 const flights = require("./controllers/flightController");
 const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
+const fs = require("fs");
 
 const app = express();
 
 app.use(json());
 
+app.use(express.urlencoded({ extended: false }))
+
 app.use("/", routes);
+
+app.get('./models/Flight.js',(req, res)=>{
+  return res.json({ flights })
+})
+
+app.post
 
 const port = process.env.PORT || 3000;
 
